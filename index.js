03d46427bea7dfa40b932a346ee4eb5abec79e60
Bhoomika-86 <dakuri.bhoomika86@gmail.com>
2025-06-03 12:15:48 +0530

    updated

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
 const app=express()
 app.listen(8080, () => {
+    mongoose.connect("mongodb://localhost:27017");
     console.log("Server Started");
 });
 
-app.get("/", (req,res) => {
+const userSchema = mongoose.Schema({
+    name : { type: String },
+    email : { type: String },
+    pass : { type: String },
+
+});
+
+const user = mongoose.model("User", userSchema);
+app.use(cors());
+app.use(express.json())
+app.get("/", async(req,res) => {
     return res.send("GOOD MORNING");
     
 });
+app.post("/register",async(req,res) => {
+    const {name,email,pass}=req.body;
+    const result = await user.insertOne({name, email, pass});//name:name etc
+    return res.json(result);
+});
+
+app.post("/login", async(req,res)=> {
+    const { email,pass } = req.body;
+    const result= await user.findOne({ email,pass});
+    if (!result) return res.json({message:"Invalid user or password"})
+        return res.json(result);
+});
+
+app.post("/products",async(req,res) => {
+    const {name,price}=req.body;
+    const result = await user.insertOne({name, price});//name:name etc
+    return res.json(result);
+});
+
 
 app.get("/greet",(req,res)=>{res.send("Greetings")})
 
